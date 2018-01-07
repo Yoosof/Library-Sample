22184b1138acec2eb1433de6f116416323b0b971
yoosof <mashayekhi.y@gmail.com>
2018-01-07 21:19:11 +0330

    refactoring DB class

---
+++ design_pattern_Library/src/com/company/Config.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by me on 07/01/2018.
+ */
+public class Config {
+    public static String DB_HOST = "localhost";
+    public static int DB_PORT = 3306;
+    public static String DB_NAME = "design_pattern_library";
+    public static String DB_USERNAME = "root";
+    public static String DB_PASS = "";
+}
--- design_pattern_Library/src/com/company/dao/DB.java
+++ design_pattern_Library/src/com/company/dao/DB.java
@@ -1,5 +1,7 @@
 package com.company.dao;
 
+import com.company.Config;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -10,7 +12,7 @@ public class DB {
 		try{
 			
 			Class.forName("com.mysql.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/design_pattern_library","root","");
+			con=DriverManager.getConnection(String.valueOf(System.out.printf("jdbc:mysql://%s:$d/%s","%s","%s", Config.DB_HOST, Config.DB_PORT, Config.DB_NAME, Config.DB_USERNAME, Config.DB_PASS)));
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
